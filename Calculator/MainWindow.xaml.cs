110275553e16336ff2443e371961a77b4a8761f4
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-16 14:14:43 +0100

    done with the UI

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        int num1;
+        int num2;
+        string result;
+        string mathChoise;
+        List<int> inputs = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 0;
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 1;
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 2;
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 3;
+        }
+
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 4;
+        }
+
+        private void b5_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 5;
+        }
+
+        private void b6_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 6;
+        }
+
+        private void b7_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 7;
+        }
+
+        private void b8_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 8;
+        }
+
+        private void b9_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text += 9;
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            mathChoise = "-";
+        }
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            mathChoise = "+";
         }
     }
 }
