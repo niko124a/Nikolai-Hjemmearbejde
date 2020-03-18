762a76f686d1d8d503ba12f9ef1a5cf1807fa5ee
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-18 14:47:28 +0100

    Added lists
    Jeg er begyndt at lave så den kan tage mere end 2 numre

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 
@@ -9,9 +10,10 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        int num1 = 0;
-        int num2 = 0;
-        string result;
+
+        List<double> numbers = new List<double>();
+        List<string> op = new List<string>();
+        private string num = "";
         string operation = "";
         List<int> inputs = new List<int>();
         public MainWindow()
@@ -21,148 +23,69 @@ namespace Calculator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10);
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10);
-                TextBox.Text = num2.ToString();
-            }
+                num += "0";
+                TextBox.Text = num;
         }
 
         private void b1_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 1;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 1;
-                TextBox.Text = num2.ToString();
-            }
-
+                num += "1";
+                TextBox.Text = num;
         }
 
         private void b2_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 2;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 2;
-                TextBox.Text = num2.ToString();
-            }
+                num += "2";
+                TextBox.Text = num;
         }
 
         private void b3_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 3;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 3;
-                TextBox.Text = num2.ToString();
-            }
+                num += "3";
+                TextBox.Text = num;
         }
 
         private void b4_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 4;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 4;
-                TextBox.Text = num2.ToString();
-            }
+                num += "4";
+                TextBox.Text = num;
         }
 
         private void b5_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 5;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 5;
-                TextBox.Text = num2.ToString();
-            }
+                num += "5";
+                TextBox.Text = num;
         }
 
         private void b6_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 6;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 6;
-                TextBox.Text = num2.ToString();
-            }
+                num += "6";
+                TextBox.Text = num;
         }
 
         private void b7_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 7;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 7;
-                TextBox.Text = num2.ToString();
-            }
+                num += "7";
+                TextBox.Text = num;
         }
 
         private void b8_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 8;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 8;
-                TextBox.Text = num2.ToString();
-            }
+                num += "8";
+                TextBox.Text = num;
         }
 
         private void b9_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + 9;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 * 10) + 9;
-                TextBox.Text = num2.ToString();
-            }
+                num += "9";
+                TextBox.Text = num;
         }
 
         private void minus_Click(object sender, RoutedEventArgs e)
         {
+            numbers.Add(Convert.ToDouble(num));
             operation = "-";
+            op.Add(operation);
             TextBox.Text = "0";
         }
         private void plus_Click(object sender, RoutedEventArgs e)
@@ -185,70 +108,33 @@ namespace Calculator
 
         private void equals_Click(object sender, RoutedEventArgs e)
         {
-            switch (operation)
-            {
-                case "+":
-                    TextBox.Text = (num1 + num2).ToString();
-                    break;
-                case "-":
-                    TextBox.Text = (num1 - num2).ToString();
-                    break;
-                case "*":
-                    TextBox.Text = (num1 * num2).ToString();
-                    break;
-                case "/":
-                    TextBox.Text = (num1 / num2).ToString();
-                    break;
-            }
+            
         }
 
         private void ClearEntry_Click(object sender, RoutedEventArgs e)
         {
             if (operation == "")
             {
-                num1 = 0;
-            }
-            else
-            {
-                num2 = 0;
+                num = "0";
             }
             TextBox.Text = "0";
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            num1 = 0;
-            num2 = 0;
+            num = "0";
             operation = "";
             TextBox.Text = "0";
         }
 
         private void Delete_OnClick(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = (num1 / 10);
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 = (num2 / 10);
-                TextBox.Text = num2.ToString();
-            }
+            
         }
 
         private void PositivNegativ_OnClick(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 *= -1;
-                TextBox.Text = num1.ToString();
-            }
-            else
-            {
-                num2 *= -1;
-                TextBox.Text = num2.ToString();
-            }
+            
         }
     }
 }
