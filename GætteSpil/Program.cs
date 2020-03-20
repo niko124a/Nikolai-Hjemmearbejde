e3e403912c99b134b52aa22aa1aab7b696fc281c
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-20 12:46:22 +0100

    Startede på gættespillet fordi jeg ikke kan få min lommeregner til at virke lige pt.

--- GætteSpil/Program.cs
+++ GætteSpil/Program.cs
@@ -4,9 +4,29 @@ namespace GætteSpil
 {
     class Program
     {
+        public static string playerGuess = Console.ReadLine();
+
         static void Main(string[] args)
         {
-            
+            Random randomNum = new Random();
+            int number = randomNum.Next(11);
+            int tries = 3;
+
+            while (true)
+            {
+                Console.WriteLine("Hello player, welcome to this EPIC game, where you are supposed to guess a number between 0 and 10");
+                Console.WriteLine("To get started, press 1");
+                var playerInput = Console.ReadKey();
+                if (playerInput.Key == ConsoleKey.D1)
+                {
+
+
+
+
+                    tries--;
+                    Console.WriteLine($"You now have {tries} left");
+                }
+            }
         }
     }
 }
