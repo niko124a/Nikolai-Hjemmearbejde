edbdcb4698bf332e995f6d99471b7fa1cfb4b7c4
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-24 11:25:36 +0100

    Laver videre på gætte spillet

--- GætteSpil/Program.cs
+++ GætteSpil/Program.cs
@@ -12,21 +12,31 @@ namespace GætteSpil
             int number = randomNum.Next(11);
             int tries = 3;
 
-            while (true)
-            {
+            
                 Console.WriteLine("Hello player, welcome to this EPIC game, where you are supposed to guess a number between 0 and 10");
                 Console.WriteLine("To get started, press 1");
+
+
+                for (int i = 0; i <= 3; i++)
+                {
+                    
+                }
                 var playerInput = Console.ReadKey();
                 if (playerInput.Key == ConsoleKey.D1)
                 {
+                    Console.WriteLine($"Please type your guess here: {playerGuess}");
 
-
+                    if (Convert.ToInt32(playerGuess) == number)
+                    {
+                        Console.WriteLine("You won!");
+                        
+                    }
 
 
                     tries--;
                     Console.WriteLine($"You now have {tries} left");
                 }
-            }
+            
         }
     }
 }
