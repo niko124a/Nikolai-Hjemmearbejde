e222e31f3daf0499e0928e014a16cf6ff62360b8
niko124a <56532129+niko124a@users.noreply.github.com>
2020-03-26 14:44:43 +0100

    Rewrote alt min kode

--- GætteSpil/Program.cs
+++ GætteSpil/Program.cs
@@ -4,9 +4,8 @@ namespace GætteSpil
 {
     class Program
     {
-        public static string playerGuess = Console.ReadLine();
 
-        static void Main(string[] args)
+        public static void Gættespil()
         {
             Random randomNum = new Random();
             int number = randomNum.Next(11);
@@ -61,6 +60,56 @@ namespace GætteSpil
                 Console.WriteLine($"You now have {tries} tries left");
                 Console.ReadKey();
                 Console.Clear();
+            }
+        }
+
+        public static string playerGuess = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Random r = new Random();
+                int val = r.Next(0, 10);
+                int guess = 0;
+                int Tries = 3;
+                Console.Clear();
+                Console.WriteLine("I'm thinking of a number between 0 and 10.");
+                
+
+                while (Tries != 0)
+                {
+                    Console.Write("Guess: ");
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.WriteLine("That's not a number.");
+                        continue;
+                    }
+
+                    if (guess < val)
+                    {
+                        Tries--;
+                        Console.WriteLine($"No, the number is higher than {guess}.");
+                    }
+                    else if (guess > val)
+                    {
+                        Tries--;
+                        Console.WriteLine($"No, the number I'm thinking is lower than {guess}.");
+                    }
+                    else
+                    {
+                        Tries = 3;
+                        Console.WriteLine("You guessed right!");
+                        Console.ReadKey();
+                    }
+
+                    if (Tries == 0)
+                    {
+                        Console.WriteLine("");
+                    }
+                }
             }
         }
     }
